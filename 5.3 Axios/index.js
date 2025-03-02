e4d4fc2728d4c310619ab9d7ac797d10d9b550d7
amir zaim <amirzaimofficial@gmail.com>
2025-03-03 07:54:34 +0800

    learn what is JSON and how to use Axios

---
+++ 5.3 Axios/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express();
+let port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.post('/', async (req, res) => {
+    console.log(req.body)
+    let typeChoice = req.body['type'];
+    let partiChoice = req.body['participants'];
+    try {
+        let response = await axios.get(`http://localhost:4000/filter?type=${typeChoice}&participants=${partiChoice}`)
+        let result = response.data[Math.floor(Math.random() * response.data.length)];
+        console.log(result);
+        res.render('index.ejs', {info: result});
+    } catch (error) {
+        console.log('Failed to make request', error.message);
+        res.render('index.ejs', {error: error.message})
+    }
+})
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('http://localhost:4000/random');
+        let result = response.data;
+        res.render('index.ejs', {info: result})
+    } catch (error) {
+        console.error('Failed to make request', error.message);
+        res.render('index.ejs', {error: error.message})
+    }
+    
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
